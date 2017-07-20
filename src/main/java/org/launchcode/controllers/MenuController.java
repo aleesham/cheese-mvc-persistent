5761addceff82000aba805d7903379b9f183bf99
avmoran92 <avmoran@wustl.edu>
2017-07-20 18:01:15 -0500

    Added a controller to route to "/menu" and a view for this path.

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -4,8 +4,11 @@ import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.groups.ConvertGroup;
 
 /**
  * Created by alees on 7/20/2017.
@@ -18,5 +21,14 @@ public class MenuController {
 
     @Autowired
     CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+        model.addAttribute
+                ("menus",
+                        menuDao.findAll()
+                );
+        return "menu/index";
+    }
 
 }
