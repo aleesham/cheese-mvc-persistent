de94fd957d8909593083324cb7c76de0fdaf566a
avmoran92 <avmoran@wustl.edu>
2017-07-20 18:11:50 -0500

    Added "add" and controller to display.

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,14 +1,13 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.groups.ConvertGroup;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by alees on 7/20/2017.
@@ -24,11 +23,16 @@ public class MenuController {
 
     @RequestMapping(value = "")
     public String index(Model model){
-        model.addAttribute
-                ("menus",
-                        menuDao.findAll()
-                );
+        model.addAttribute("title", "Menus");
+        model.addAttribute("menus", menuDao.findAll());
         return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model){
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
     }
 
 }
