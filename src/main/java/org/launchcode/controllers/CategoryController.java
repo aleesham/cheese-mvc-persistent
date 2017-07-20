b8a18a74f38990c58dd8ba87624f7f1e93c14d28
avmoran92 <avmoran@wustl.edu>
2017-07-20 15:12:33 -0500

    Finished part 2

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,59 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by alees on 7/19/2017.
+ */
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title",
+                "Categories");
+
+        return "category/index";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String displayAddForm(Model model) {
+
+        model.addAttribute(new Category());
+        model.addAttribute("title",
+                "Add Category");
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String displayAddForm(Model model, @ModelAttribute @Valid Category
+            category, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+
+            categoryDao.save(category);
+        return "redirect:";
+    }
+
+}
